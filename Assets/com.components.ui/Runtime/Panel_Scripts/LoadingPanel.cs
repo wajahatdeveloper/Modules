8f2a4f348b67cab9b453295f46d55da10bac258c
WajahatDeveloper <wajahatdeveloperqs@gmail.com>
2021-12-07 17:47:04 +0500

    Updates

---
+++ Assets/com.components.ui/Runtime/Panel_Scripts/LoadingPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class LoadingPanel : SingletonBehaviour<LoadingPanel>
+{
+    public GameObject loadingPanel;
+
+    public UnityEvent onClose;
+
+    public void Show()
+    {
+        loadingPanel.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        loadingPanel.SetActive(false);
+        onClose?.Invoke();
+        onClose?.RemoveAllListeners();
+    }
+}
---
+++ Assets/com.components.ui/Runtime/Panel_Scripts/PopupMessage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PopupMessage : SingletonBehaviour<PopupMessage>
+{
+    public Text messageText;
+    public GameObject messagePanel;
+
+    public UnityEvent onClose;
+
+    private bool _isAuto;
+
+    public void ShowAuto(string message)
+    {
+        _isAuto = true;
+        Show(message);
+    }
+
+    public void Show(string message)
+    {
+        messageText.text = message;
+        messagePanel.SetActive(true);
+    }
+
+    public void HideAuto()
+    {
+        _isAuto = false;
+        Hide();
+    }
+
+    public void Hide()
+    {
+        if (_isAuto) return;
+        messagePanel.SetActive(false);
+        onClose?.Invoke();
+        onClose?.RemoveAllListeners();
+    }
+}
