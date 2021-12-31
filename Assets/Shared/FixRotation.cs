ec033f6ae833ae564558600dea7138812ef838d9
WajahatDeveloper <wajahatdeveloperqs@gmail.com>
2021-12-31 12:06:40 +0500

    updates

---
+++ Assets/Shared/FixRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FixRotation : MonoBehaviour
+{
+    public Vector3 offset;
+    
+    private Vector3 startingRotation;
+    
+    void Awake()
+    {
+        startingRotation = transform.eulerAngles;
+    }
+    void LateUpdate()
+    {
+        transform.eulerAngles = startingRotation + offset;
+    }
+}
