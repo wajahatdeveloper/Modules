ab82115084ff2fa9175dfd4b06cb34be316197cd
WajahatDeveloper <wajahatdeveloperqs@gmail.com>
2022-05-10 12:05:31 +0500

    updates

--- Assets/FunctionDemo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Reflection;
-using UnityEditor;
-using UnityEngine;
-
-public class FunctionDemo : MonoBehaviour
-{
-}
---
+++ Assets/com.components.ui/Runtime/Panel_Scripts/LineMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class LineMessage : SingletonBehaviour<LineMessage>
+{
+    public GameObject messagePrefab;
+
+    public void Show(string message, string titleString = "", float time = 1.0f)
+    {
+        var messageLine = Instantiate(messagePrefab,transform);
+        var titleText = messageLine.transform.GetChild(0).GetComponent<Text>(); // Title
+        var messageText = messageLine.transform.GetChild(0).GetComponent<Text>(); // Message
+        
+        messageText.text = message;
+        titleText.text = titleString;
+        
+        Destroy(messageLine,time);
+    }
+}
