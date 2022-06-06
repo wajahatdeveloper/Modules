7d7914af4c8d64cdf8849026acde026257853717
WajahatDeveloper <wajahatdeveloperqs@gmail.com>
2022-06-06 11:46:20 +0500

    updated uihelperwindow

--- Assets/com.components.ui/Editor/UIHelperWindow.cs
+++ Assets/com.components.ui/Editor/UIHelperWindow.cs
@@ -7,28 +7,51 @@ using UnityEngine;
 
 public class UIHelper : EditorWindow
 {
-    protected Vector2 scrollPosition;
-
     protected static Dictionary<string, GameObject> items = new Dictionary<string, GameObject>();
+    
+    protected Vector2 scrollPosition;
 
     [MenuItem("Hub/UI Helper")]
     public static void Init()
     {
-        items.Clear();
-        var lst = Resources.LoadAll<GameObject>("UIWidgets");
-        foreach (GameObject o in lst) { items.Add(o.name,o); }
-        items.Add("-",null);
-        lst = Resources.LoadAll<GameObject>("UIWidgets_Scrolls");
-        foreach (GameObject o in lst) { items.Add(o.name,o); }
-        items.Add("--",null);
-        lst = Resources.LoadAll<GameObject>("UIWidgets_Panels");
-        foreach (GameObject o in lst) { items.Add(o.name,o); }
+        UpdateList();
 
         Resources.UnloadUnusedAssets();
         
         var window = GetWindow<UIHelper>();
         window.minSize = new Vector2(250f, 200f);
         window.Show();
+    }
+
+    private static void UpdateList()
+    {
+        items.Clear();
+        var lst = Resources.LoadAll<GameObject>("UIWidgets");
+        foreach (GameObject o in lst)
+        {
+            items.Add(o.name, o);
+        }
+
+        items.Add("-", null);
+        lst = Resources.LoadAll<GameObject>("UIWidgets_Scrolls");
+        foreach (GameObject o in lst)
+        {
+            items.Add(o.name, o);
+        }
+
+        items.Add("--", null);
+        lst = Resources.LoadAll<GameObject>("UIWidgets_Panels");
+        foreach (GameObject o in lst)
+        {
+            items.Add(o.name, o);
+        }
+    }
+
+    private void OnFocus()
+    {
+        UpdateList();
+
+        Resources.UnloadUnusedAssets();
     }
 
     protected virtual void OnGUI()
