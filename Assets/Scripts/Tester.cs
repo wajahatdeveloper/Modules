d75927f5e79d5f24c4d3706b3ff881a32965e534
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2022-09-19 18:40:24 +0500

    KeyValueDictionary

--- Assets/Scripts/Tester.cs
+++ Assets/Scripts/Tester.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Tester : MonoBehaviour
--- Assets/com.datastructures/Runtime/KeyValueDictionary.cs
+++ Assets/com.datastructures/Runtime/KeyValueDictionary.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 [System.Serializable]
 public class KeyValueDictionary_Item
 {
+    public KeyValueDictionary_Item()
+    {
+    }
+
+    public KeyValueDictionary_Item(string key, string value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+
     public string key;
     public string value;
 }
 
 [System.Serializable]
-public class KeyValueDictionary : IEnumerable<KeyValueDictionary_Item>
+public class KeyValueDictionary : IEnumerable<KeyValueDictionary_Item>, ICollection<KeyValueDictionary_Item>, IEnumerable, IList<KeyValueDictionary_Item>
 {
     public List<KeyValueDictionary_Item> items;
-    
+
+    public int Count => ((ICollection<KeyValueDictionary_Item>)items).Count;
+
+    public bool IsReadOnly => ((ICollection<KeyValueDictionary_Item>)items).IsReadOnly;
+
+    public KeyValueDictionary_Item this[int index] { get => ((IList<KeyValueDictionary_Item>)items)[index]; set => ((IList<KeyValueDictionary_Item>)items)[index] = value; }
     public string this[string key]
     {
         get => GetValue(key);
@@ -38,5 +53,45 @@ public class KeyValueDictionary : IEnumerable<KeyValueDictionary_Item>
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
+    }
+
+    public void Add(KeyValueDictionary_Item item)
+    {
+        ((ICollection<KeyValueDictionary_Item>)items).Add(item);
+    }
+
+    public void Clear()
+    {
+        ((ICollection<KeyValueDictionary_Item>)items).Clear();
+    }
+
+    public bool Contains(KeyValueDictionary_Item item)
+    {
+        return ((ICollection<KeyValueDictionary_Item>)items).Contains(item);
+    }
+
+    public void CopyTo(KeyValueDictionary_Item[] array, int arrayIndex)
+    {
+        ((ICollection<KeyValueDictionary_Item>)items).CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(KeyValueDictionary_Item item)
+    {
+        return ((ICollection<KeyValueDictionary_Item>)items).Remove(item);
+    }
+
+    public int IndexOf(KeyValueDictionary_Item item)
+    {
+        return ((IList<KeyValueDictionary_Item>)items).IndexOf(item);
+    }
+
+    public void Insert(int index, KeyValueDictionary_Item item)
+    {
+        ((IList<KeyValueDictionary_Item>)items).Insert(index, item);
+    }
+
+    public void RemoveAt(int index)
+    {
+        ((IList<KeyValueDictionary_Item>)items).RemoveAt(index);
     }
 }
