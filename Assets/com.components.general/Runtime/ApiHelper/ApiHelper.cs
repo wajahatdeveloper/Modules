736ef785c671c2b685487d20db343ae8c70adf3c
wajahatdeveloper <wajahatdeveloperqs@gmail.com>
2022-10-31 14:35:39 +0500

    apiHelperUpdate

--- Assets/com.components.general/Runtime/ApiHelper/ApiHelper.cs
+++ Assets/com.components.general/Runtime/ApiHelper/ApiHelper.cs
@@ -8,28 +8,65 @@ using UnityEngine.Networking;
 public class ApiHelper : MonoBehaviour
 {
     public static bool DisableDefaultErrorHandling = true;
-    
-    public static IEnumerator Get(string url, Action<string> success, Action<string> error, string payload , Dictionary<string,string> headers = null)
+    private static GameObject coroutineHelper;
+
+    public static ApiHelper CoroutineHelper { get
+        {
+            if (coroutineHelper == null)
+            {
+                coroutineHelper = new GameObject("CoroutineHelper");
+                coroutineHelper.AddComponent<ApiHelper>();
+            }
+            return coroutineHelper.GetComponent<ApiHelper>(); 
+        }
+        private set => coroutineHelper = value.gameObject; }
+
+    public static void Get(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
     {
-        using UnityWebRequest webRequest = UnityWebRequest.Get( url );
+        CoroutineHelper.StartCoroutine(Get_CR(url, success, error, payload, headers));
+    }
+
+    public static void Post(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
+    {
+        CoroutineHelper.StartCoroutine(Post_CR(url, success, error, payload, headers));
+	}
+
+	public static void Patch(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
+    {
+        CoroutineHelper.StartCoroutine(Patch_CR(url, success, error, payload, headers));
+	}
+
+	public static void Delete(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
+    {
+        CoroutineHelper.StartCoroutine(Delete_CR(url, success, error, payload, headers));
+	}
+
+	public static void DownloadImage(string mediaUrl, Action<Texture2D> success, Action<string> error)
+    {
+        CoroutineHelper.StartCoroutine(DownloadImage_CR(mediaUrl, success, error));
+	}
+
+	public static IEnumerator Get_CR(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
+    {
+        using UnityWebRequest webRequest = UnityWebRequest.Get(url);
 
         if (headers != null)
         {
-            foreach (KeyValuePair<string,string> pair in headers)
+            foreach (KeyValuePair<string, string> pair in headers)
             {
-                webRequest.SetRequestHeader(pair.Key,pair.Value);
+                webRequest.SetRequestHeader(pair.Key, pair.Value);
             }
         }
-        
-        webRequest.SetRequestHeader( "Content-Type", "application/json" );
-        
+
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
         if (payload != "")
         {
-            byte[] bodyRaw = Encoding.UTF8.GetBytes( payload );
-            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw( bodyRaw );
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(payload);
+            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
             webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         }
-        
+
         Debug.Log($"Accessing Endpoint {url} with GET Request");
         yield return webRequest.SendWebRequest();
 
@@ -41,7 +78,7 @@ public class ApiHelper : MonoBehaviour
                 if (!DisableDefaultErrorHandling)
                 {
                     Debug.Log("Error Occured for Endpoint : " + url);
-                    Debug.LogError("Error Body : " + webRequest.error );
+                    Debug.LogError("Error Body : " + webRequest.error);
                     error(webRequest.error);
                 }
                 else
@@ -51,33 +88,33 @@ public class ApiHelper : MonoBehaviour
                 break;
             case UnityWebRequest.Result.Success:
                 Debug.Log("Response Received for Endpoint : " + url);
-                Debug.Log( "Response Body : " + webRequest.downloadHandler.text );
+                Debug.Log("Response Body : " + webRequest.downloadHandler.text);
                 success(webRequest.downloadHandler.text);
                 break;
         }
     }
-    
-    public static IEnumerator Post(string url, Action<string> success, Action<string> error, string payload, Dictionary<string,string> headers = null)
+
+    public static IEnumerator Post_CR(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
     {
         using UnityWebRequest webRequest = new UnityWebRequest(url, "POST");
-        
+
         if (headers != null)
         {
-            foreach (KeyValuePair<string,string> pair in headers)
+            foreach (KeyValuePair<string, string> pair in headers)
             {
-                webRequest.SetRequestHeader(pair.Key,pair.Value);
+                webRequest.SetRequestHeader(pair.Key, pair.Value);
             }
         }
-        
-        webRequest.SetRequestHeader( "Content-Type", "application/json" );
-            
+
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
         if (payload != "")
         {
-            byte[] bodyRaw = Encoding.UTF8.GetBytes( payload );
-            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw( bodyRaw );
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(payload);
+            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
             webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         }
-            
+
         Debug.Log($"Accessing Endpoint {url} with POST Request");
         yield return webRequest.SendWebRequest();
 
@@ -89,7 +126,7 @@ public class ApiHelper : MonoBehaviour
                 if (!DisableDefaultErrorHandling)
                 {
                     Debug.Log("Error Occured for Endpoint : " + url);
-                    Debug.LogError("Error Body : " + webRequest.error );
+                    Debug.LogError("Error Body : " + webRequest.error);
                     error(webRequest.error);
                 }
                 else
@@ -99,13 +136,13 @@ public class ApiHelper : MonoBehaviour
                 break;
             case UnityWebRequest.Result.Success:
                 Debug.Log("Response Received for Endpoint : " + url);
-                Debug.Log( "Response Body : " + webRequest.downloadHandler.text );
+                Debug.Log("Response Body : " + webRequest.downloadHandler.text);
                 success(webRequest.downloadHandler.text);
                 break;
         }
     }
 
-    public static IEnumerator Patch(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
+    public static IEnumerator Patch_CR(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
     {
         using UnityWebRequest webRequest = new UnityWebRequest(url, "PATCH");
 
@@ -153,27 +190,27 @@ public class ApiHelper : MonoBehaviour
         }
     }
 
-    public static IEnumerator Delete(string url, Action<string> success, Action<string> error, string payload, Dictionary<string,string> headers = null)
+    public static IEnumerator Delete_CR(string url, Action<string> success, Action<string> error, string payload, Dictionary<string, string> headers = null)
     {
         using UnityWebRequest webRequest = new UnityWebRequest(url, "DELETE");
-        
+
         if (headers != null)
         {
-            foreach (KeyValuePair<string,string> pair in headers)
+            foreach (KeyValuePair<string, string> pair in headers)
             {
-                webRequest.SetRequestHeader(pair.Key,pair.Value);
+                webRequest.SetRequestHeader(pair.Key, pair.Value);
             }
         }
-        
-        webRequest.SetRequestHeader( "Content-Type", "application/json" );
-            
+
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
         if (payload != "")
         {
-            byte[] bodyRaw = Encoding.UTF8.GetBytes( payload );
-            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw( bodyRaw );
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(payload);
+            webRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
             webRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
         }
-            
+
         Debug.Log($"Accessing Endpoint {url} with DELETE Request");
         yield return webRequest.SendWebRequest();
 
@@ -185,7 +222,7 @@ public class ApiHelper : MonoBehaviour
                 if (!DisableDefaultErrorHandling)
                 {
                     Debug.Log("Error Occured for Endpoint : " + url);
-                    Debug.LogError("Error Body : " + webRequest.error );
+                    Debug.LogError("Error Body : " + webRequest.error);
                     error(webRequest.error);
                 }
                 else
@@ -195,18 +232,18 @@ public class ApiHelper : MonoBehaviour
                 break;
             case UnityWebRequest.Result.Success:
                 Debug.Log("Response Received for Endpoint : " + url);
-                Debug.Log( "Response Body : " + webRequest.downloadHandler.text );
+                Debug.Log("Response Body : " + webRequest.downloadHandler.text);
                 success(webRequest.downloadHandler.text);
                 break;
         }
     }
-    
-    public static IEnumerator DownloadImage( string mediaUrl, Action<Texture2D> success, Action<string> error )
+
+    public static IEnumerator DownloadImage_CR(string mediaUrl, Action<Texture2D> success, Action<string> error)
     {
-        UnityWebRequest request = UnityWebRequestTexture.GetTexture( mediaUrl );
-        
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(mediaUrl);
+
         yield return request.SendWebRequest();
-        
+
         switch (request.result)
         {
             case UnityWebRequest.Result.ConnectionError:
@@ -215,7 +252,7 @@ public class ApiHelper : MonoBehaviour
                 if (!DisableDefaultErrorHandling)
                 {
                     Debug.Log("Error Occured for Endpoint : " + mediaUrl);
-                    Debug.LogError("Error Body : " + request.error );
+                    Debug.LogError("Error Body : " + request.error);
                     error(request.error);
                 }
                 else
@@ -225,7 +262,7 @@ public class ApiHelper : MonoBehaviour
                 break;
             case UnityWebRequest.Result.Success:
                 Debug.Log("Response Received for Endpoint : " + mediaUrl);
-                Debug.Log( "Response Body : " + request.downloadHandler.text );
+                Debug.Log("Response Body : " + request.downloadHandler.text);
                 var texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
                 success(texture);
                 break;
