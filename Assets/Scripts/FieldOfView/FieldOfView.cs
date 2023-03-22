c7a3534ba7fb3842f70f20d45c9265e66cc18dda
WajahatDeveloper <wajahatdeveloperqs@gmail.com>
2023-03-22 16:59:41 +0500

    fov update

--- Assets/Scripts/FieldOfView/FieldOfView.cs
+++ Assets/Scripts/FieldOfView/FieldOfView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,14 @@ public class FieldOfView : MonoBehaviour
     public LayerMask obstacleMask;
 
     public List<Transform> visibleTargets = new List<Transform>();
+
+    public Action<Transform> onTargetEnteredField;
+
+    public Action onFieldBecameEmpty;
+    public Action onFieldBecameActive;
+
+    private bool hasTargets = false;
+    private bool hadTargets = false;
 
     private void Start()
     {
@@ -38,6 +47,8 @@ public class FieldOfView : MonoBehaviour
     private void FindVisibleTargets()
     {
         visibleTargets.Clear();
+        hasTargets = false;
+
         var targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius,targetMask);
         foreach (Collider target in targetsInViewRadius)
         {
@@ -54,8 +65,49 @@ public class FieldOfView : MonoBehaviour
                 if (!Physics.Raycast(transform.position,dirToTarget,distanceToTarget,obstacleMask))
                 {
                     visibleTargets.Add(target.transform);
+                    onTargetEnteredField?.Invoke(target.transform);
+                    hasTargets = true;
                 }
             }
         }
+
+        if (hadTargets)
+        {
+            if (!hasTargets)
+            {
+                onFieldBecameEmpty?.Invoke();
+            }
+        }
+
+        if (!hadTargets)
+        {
+            if (hasTargets)
+            {
+                onFieldBecameActive?.Invoke();
+            }
+        }
+
+        hadTargets = hasTargets;
+
+        SortByObject(transform);
+    }
+
+    private void SortByObject(Transform target)
+    {
+        visibleTargets.Sort(delegate (Transform t1, Transform t2) {
+            return
+                Vector3.Distance(
+                    t1.position,
+                    target.position
+                )
+
+                .CompareTo(
+                    Vector3.Distance(
+                        t2.position,
+                        target.position
+                    )
+                )
+            ;
+        });
     }
 }
