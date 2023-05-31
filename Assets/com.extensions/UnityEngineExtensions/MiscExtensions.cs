c26113431c12781d1d5f26f2c700eda8461a9237
wajahat <wajahatdeveloperqs@gmail.com>
2023-05-31 14:12:42 +0500

    Add ponter event systemt ext

---
+++ Assets/com.extensions/UnityEngineExtensions/MiscExtensions.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public static class MiscExtensions
+{
+    #region IsPointerOnUIElement
+
+    public static bool IsPointerOverUIElement(this EventSystem eventSystem, GameObject gameObject)
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults(eventSystem),gameObject);
+    }
+    
+    private static bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults, GameObject gameObject)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == LayerMask.NameToLayer("UI") && 
+                curRaysastResult.gameObject == gameObject)
+                return true;
+        }
+        return false;
+    }
+    
+    private static List<RaycastResult> GetEventSystemRaycastResults(EventSystem eventSystem)
+    {
+        PointerEventData eventData = new PointerEventData(eventSystem);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        eventSystem.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
+
+    #endregion
+}
