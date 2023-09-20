865b4b2895a91e9b27f401ca0ee145c27796e73e
wajahat <wajahatdeveloperqs@gmail.com>
2023-09-20 18:08:12 +0500

    updates

---
+++ Assets/Scripts/AutoGenerated/SortingLayers.cs
@@ -0,0 +1,7 @@
+//This class is auto-generated do not modify (TagsLayersScenesBuilder.cs) - blog.almostlogical.com
+public class SortingLayers
+{
+	public const string DEFAULT = "Default";
+
+	public const int DEFAULT_INT = 0;
+}
